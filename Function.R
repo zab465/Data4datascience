7fce1d51a1ad43095f003e34aafbda26738a7153
zab465 <valentina.laugeri@gmail.com>
2023-10-25 18:01:54 +0100

    new function with added argument

--- Function.R
+++ Function.R
@@ -1,21 +1,23 @@
-#The function designed below will be used for the "swiss" dataset 
-data("swiss")
-View(swiss)
+#The function designed below will be used for the "trees" dataset 
+#contains information about 
+data("trees")
 
-#The function will return the return head or tail for the third column in the data set 
-#if the second argument is specified
-#else it will return the summary like the original function 
-my_function <- function(data, action){
-  if (ncol(data) < 2) {
-    stop 
-  } else if (action == "head") {
-    return(head(data[3]))
-  } else if (action == "tail") {
-    return(tail(data[3]))
-  } else {
-    return(summary(data[3]))
-  } 
+#The function will return the values of the first column in centimeters, from inches
+my_function <- function(data, action) {
+  if (action == "metre"){
+    mtr <- data[, 1] * 0.0254
+    return(mtr)
+    }else if (action == "mm"){
+      mm <- data[, 1] * 25.4
+      return(mm)
+      }else if (action == "group"){
+        split_data <- split(data, data[, 1])
+        return(split_data)
+        }else{
+          cm <- data[, 1]*2.54
+          return(cm)
+        }
 }
 
-#testing function 
-my_function(swiss, "head")
+
+my_function(trees, "group")
